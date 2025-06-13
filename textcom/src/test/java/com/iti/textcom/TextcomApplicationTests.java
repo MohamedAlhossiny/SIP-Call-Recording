354874766183e3c318ee3edfc88c0f96653eb250
omar tarek <58843923+omar-eng@users.noreply.github.com>
2025-06-13 21:57:34 +0300

    added test

---
+++ textcom/src/test/java/com/iti/textcom/TextcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iti.textcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
